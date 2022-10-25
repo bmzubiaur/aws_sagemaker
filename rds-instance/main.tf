cf4501ab97f833ef17c71709e2f0811f148b3380
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 14:06:51 -0500

    Update main.tf

--- rds-instance/main.tf
+++ rds-instance/main.tf
@@ -4,5 +4,6 @@ resource "aws_db_instance" "default" {
   instance_class       = "db.t3.small"
   username             = var.database_username
   password             = var.database_password
+  db_subnet_group_name  = aws_db_subnet_group.main.id
   skip_final_snapshot  = true
 }
