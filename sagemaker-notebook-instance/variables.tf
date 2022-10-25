cf70dd7477d4585bfb083125986b8a704d15e1af
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 11:04:40 -0500

    Update variables.tf

--- sagemaker-notebook-instance/variables.tf
+++ sagemaker-notebook-instance/variables.tf
@@ -2,3 +2,8 @@ variable "database_username" {
   type        = string
   description = "database username"
 }
+
+variable "database_password" {
+  type        = string
+  description = "database password"
+}
