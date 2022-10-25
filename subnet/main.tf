aa4f870f44009959eb03cd3acac8120460769010
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 15:52:28 -0500

    Update main.tf

--- subnet/main.tf
+++ subnet/main.tf
@@ -6,16 +6,18 @@ resource "aws_vpc" "main" {
 #Create a Subnet
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "Main"
-  }
+  cidr_block = "10.0.0.0/24"
 }
+
+#Create a Subnet
+resource "aws_subnet" "sec" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
 
 #Create a DB Subnet group
 resource "aws_db_subnet_group" "db_main" {
   name       = "db_main"
-  subnet_ids = [aws_subnet.main.id]
+  subnet_ids = [aws_subnet.main.id, aws_subnet.sec.id]
 }
