19081cc92f80ee9db7644741d7b44be95ae08ca3
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 11:42:56 -0500

    Update main.tf

--- rds-instance/main.tf
+++ rds-instance/main.tf
@@ -1,6 +1,5 @@
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
-  db_name              = "mydb"
   engine               = "sqlserver-ex"
   instance_class       = "db.t3.small"
   username             = var.database_username
