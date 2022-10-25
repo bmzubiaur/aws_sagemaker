eb216833944c2fe574206e315f6b0210d296e0de
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 11:10:51 -0500

    Update main.tf

--- rds-instance/main.tf
+++ rds-instance/main.tf
@@ -5,7 +5,5 @@ resource "aws_db_instance" "default" {
   instance_class       = "db.t3.small"
   username             = var.database_username
   password             = var.datanase_password
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  tags = 
+  tags                 = {var.tag1} 
 }
