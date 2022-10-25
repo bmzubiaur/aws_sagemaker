88bda7f6d82ef9be250e1a56e1cbb44d71c6cae4
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 15:37:22 -0500

    Update main.tf

--- sagemaker-notebook-instance/main.tf
+++ sagemaker-notebook-instance/main.tf
@@ -2,6 +2,7 @@ resource "aws_sagemaker_notebook_instance" "ni" {
   name          = "my-notebook-instance"
   role_arn      = aws_iam_role.role.arn
   instance_type = "ml.t2.medium"
+  subnet_id     = var.subnet_id
 
   tags = {
     Name = "foo"
