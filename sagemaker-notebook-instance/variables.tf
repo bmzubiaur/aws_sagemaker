f14137fe5c25840af2f00ee4088b0d985c76fe45
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 16:42:46 -0500

    Update variables.tf

--- sagemaker-notebook-instance/variables.tf
+++ sagemaker-notebook-instance/variables.tf
@@ -2,3 +2,8 @@ variable "subnet_id" {
   type        = string
   description = "The ID of the subnets to use for the notebook"
 }
+
+variable "vpc_default_sec_group" {
+  type        = string
+  description = "The VPC's default security group"
+}
