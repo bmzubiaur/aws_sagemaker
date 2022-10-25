aa38ed051cbfbe669132e160a209ac0cdf194b96
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-25 14:57:12 -0500

    Update variables.tf

--- rds-instance/variables.tf
+++ rds-instance/variables.tf
@@ -7,3 +7,8 @@ variable "database_password" {
   type        = string
   description = "database password"
 }
+
+variable "db_subnet_id" {
+  type        = string
+  description = "The ID of the DB subnets group"
+}
